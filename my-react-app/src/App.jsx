de27662bf586369c990d93c33bf084819acb9a80
justinburks19 <72533878+justinburks19@users.noreply.github.com>
2025-10-08 17:48:09 -0400

    First commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <button className="btn btn-primary">A Bootstrap Button</button>
+        <p>Test</p>
+      </div>
+    </>
+  )
+}
+
+export default App
